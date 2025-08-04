7f58f024018433b70633345c5348a22c82f8f295
usernameak <usernameak@protonmail.com>
2025-08-04 07:43:39 +0200

    xsr1app: avoid out-of-range in argv

--- xsr1/xsr1app/source/xsr1app/app.d
+++ xsr1/xsr1app/source/xsr1app/app.d
@@ -4,6 +4,10 @@ import xsr1core.base;
 import std.stdio;
 
 int main(string[] args) {
+    if (args.length != 3) {
+        stderr.writeln("usage: xsr1app <input_filename> <output_filename>");
+        return 1;
+    }
     File f = File(args[1], "rb");
     File wf = File(args[2], "wb");
     parseImage(f, wf);
